e91341f72b56f7050686a1e71b13eba9ab2152fc
https://github.com/settings/admin <tafairry520@gmail.com>
2022-12-05 15:04:30 +0800

    20221205_Vue lesson, Router add new page

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/newpage',
+    name: '新增頁面',
+    component: () => import('../views/NewPage.vue'),
+  },
 ];
 
 const router = createRouter({
