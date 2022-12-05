c34df4efd43266e89271774304566f2065fbae15
https://github.com/settings/admin <tafairry520@gmail.com>
2022-12-05 16:07:07 +0800

    20221205_Vue lesson, Nested routes

---
+++ src/views/ComponentA.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card border-primary mb-3" style="max-width: 18rem">
+    <div class="card-header">元件 A Header</div>
+    <div class="card-body text-primary">
+      <h5 class="card-title">Primary card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/NewPage.vue
+++ src/views/NewPage.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    新增頁面
+  <div class="h1">新增頁面</div>
+  <div class="row">
+    <div class="col-4">
+      <div class="list-group">
+        <router-link to="/newpage/a"
+          class="list-group-item list-group-item-action"
+        >
+          元件A
+        </router-link>
+        <router-link to="/newpage/b"
+          class="list-group-item list-group-item-action"
+        >
+          元件B
+        </router-link>
+      </div>
+    </div>
+    <div class="col-8">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
