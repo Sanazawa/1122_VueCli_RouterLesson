ed56d6f48c5403dcced1cab96bb58817fa023c38
https://github.com/settings/admin <tafairry520@gmail.com>
2022-12-09 13:59:32 +0800

    20221209_Vue lesson, Dynamic route demo

---
+++ src/views/DynamicRouter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  created() {
+    // console.log(this.$route.params.id);
+    // 動態網址的範例，取得從網址上輸入的id作為下面api的seed
+    const seed = this.$route.params.id;
+    axios.get(`https://randomuser.me/api/?seed=${seed}`).then((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
