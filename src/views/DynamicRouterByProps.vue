a39d8a2e6698bb010bd7c191b3153aff5d8dbfb7
https://github.com/settings/admin <tafairry520@gmail.com>
2022-12-09 14:31:29 +0800

    20221209_Vue lesson, Dynamic router by Props

---
+++ src/views/DynamicRouterByProps.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  props: ['id'],
+  created() {
+    console.log('props', this.id);
+    // 動態網址的範例，取得從網址上輸入的id作為下面api的seed
+    const seed = this.id;
+    axios.get(`https://randomuser.me/api/?seed=${seed}`).then((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
